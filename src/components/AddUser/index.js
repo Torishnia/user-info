0ee8a55fb50f4bf011dc0a568420b7722a38f696
Valeri <valeri.torishnya@gmail.com>
2023-08-29 16:26:22 +0300

    add component Card

--- src/components/AddUser/index.js
+++ src/components/AddUser/index.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import Card from '../UI/Card/index';
 import styles from './index.module.css';
 
 const AddUser = ({ onSave }) => {
@@ -29,35 +30,37 @@ const AddUser = ({ onSave }) => {
   }
 
   return (
-    <form onSubmit={submitHandler}>
-      <div>
-        <p>
-          <label>Username</label>
-          <input
-            id='username'
-            type='text'
-            value={enteredUsername}
-            onChange={(event) => 
-              inputChangeHandler('username', event.target.value)
-            }
-          />
-        </p>
-        <p>
-          <label>Age (Years)</label>
-          <input
-            id='age'
-            type='number'
-            value={enteredAge}
-            onChange={(event) => 
-              inputChangeHandler('age', event.target.value)
-            }
-          />
-        </p>
-      </div>
-      <div>
-        <button>Add User</button>
-      </div>
-    </form>
+    <Card className={styles.input}>
+      <form onSubmit={submitHandler}>
+        <div>
+          <p>
+            <label htmlFor='username'>Username</label>
+            <input
+              id='username'
+              type='text'
+              value={enteredUsername}
+              onChange={(event) => 
+                inputChangeHandler('username', event.target.value)
+              }
+            />
+          </p>
+          <p>
+            <label htmlFor='age'>Age (Years)</label>
+            <input
+              id='age'
+              type='number'
+              value={enteredAge}
+              onChange={(event) => 
+                inputChangeHandler('age', event.target.value)
+              }
+            />
+          </p>
+        </div>
+        <div>
+          <button type='submit'>Add User</button>
+        </div>
+      </form>
+    </Card>
   )
 }
 
